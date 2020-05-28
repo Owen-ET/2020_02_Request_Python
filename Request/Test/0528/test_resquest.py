be2dcf310e3a5eabf6ce57643fac7272927c6eb3
zhangchang <zhangchanget@126.com>
2020-05-28 17:46:17 +0800

    20200528yaml数据

--- Request/Test/0528/test_resquest.py
+++ Request/Test/0528/test_resquest.py
@@ -6,22 +6,45 @@
 
 import unittest
 import requests
+import os
+import yaml
+from ddt import ddt, data, file_data, unpack
 
+@ddt
 class TestResquest(unittest.TestCase):
+
+    def yamlData(self):
+        '''获取yaml数据'''
+        self.path = os.path.dirname(os.path.abspath(__file__))
+        # yaml路径
+        self.yamlPath = self.path + "/data/case_data.yaml"
+        # 获取yaml数据
+        with open(self.yamlPath, 'r', encoding='utf-8') as file:
+            data = yaml.load(file)
+        return data
 
     # @unittest.skip("不看")
     def test_request01(self):
-        url = 'https://ascendas.17mine.cn/basic/pick/selectPage'
-        headers = {'Authorization':'eyJhbGciOiJIUzI1NiJ9.eyJuZWVkRWRpdCI6LTEsImxvZ2luVGltZSI6MTU5MDYyOTYwMTU0NSwibG9naW5XYXkiOjEsInVzZXJOYW1lIjoi5byg55WFIiwidXNlcklkIjoiMTI1NDI5NDE4NzAzODM0NzI2NCIsImxvZ2luU291cmNlIjotMSwiYWNjb3VudCI6IjEzNjQyMDQwNjMxIiwiZXhwIjoxNTkwNjcyODAxfQ.qydhemA3sGfrBuHFWcTi8OdaOcm7hvIpgErtkQ2OVBo'}
-        payload = {
-            'pageNum': '1',
-            'pageSize': '1',
-            'user_id': '1254294187038347264',
-            'userId': '1254294187038347264',
-            'infos_id': '1207504682260500480',
-            'infoId': '1207504682260500480'}
+        #==========================旧传参写法========================
+        # url = 'https://ascendas.17mine.cn/basic/pick/selectPage'
+        # headers = {'Authorization':'eyJhbGciOiJIUzI1NiJ9.eyJuZWVkRWRpdCI6LTEsImxvZ2luVGltZSI6MTU5MDYyOTYwMTU0NSwibG9naW5XYXkiOjEsInVzZXJOYW1lIjoi5byg55WFIiwidXNlcklkIjoiMTI1NDI5NDE4NzAzODM0NzI2NCIsImxvZ2luU291cmNlIjotMSwiYWNjb3VudCI6IjEzNjQyMDQwNjMxIiwiZXhwIjoxNTkwNjcyODAxfQ.qydhemA3sGfrBuHFWcTi8OdaOcm7hvIpgErtkQ2OVBo'}
+        # payload = {
+        #     'pageNum': '1',
+        #     'pageSize': '1',
+        #     'user_id': '1254294187038347264',
+        #     'userId': '1254294187038347264',
+        #     'infos_id': '1207504682260500480',
+        #     'infoId': '1207504682260500480'}
+        #==========================================================
+
+        # =======================yaml写法①=========================
+        case01 = self.yamlData()['case01']
+        url = case01['url']
+        payload = case01['payload']
+        headers = case01['headers']
+
         r = requests.post(url,params=payload,headers=headers).json()
-        self.assertEqual(r['data']['records'][0]['stockOutName'],"西青仓库1")
+        self.assertEqual(r['data']['records'][0]['stockOutName'],"0506测试仓库")
 
     # @unittest.skipIf(2>1,"大于不执行")
     def test_request02(self):
